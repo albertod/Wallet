6f4e00699044ecf41cdd0ab0e080e4b780a62b1a
Alberto Di Martino <albertodmartino@gmail.com>
2015-06-01 15:10:15 -0500

    Initial Commit

---
+++ Wallet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wallet
+//
+//  Created by Alberto Di Martino on 6/1/15.
+//  Copyright (c) 2015 DiMartino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
